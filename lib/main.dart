45514768434ccdc9c7d9dca4d74b1e9cacbd8012
Julipoli99 <Polimenijulian9@gmail.com>
2024-05-06 18:26:12 -0300

    Agregado de botones inferiores con libreria google_nav_bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_de_estacionamiento/presentations/screens/home.dart';
 import 'package:app_de_estacionamiento/presentations/screens/screen_menu_lateral.dart';
+import 'package:app_de_estacionamiento/presentations/widgets/my_bottom_navbar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ScreenMenuLateral(),
+        body: MyButtomNavbar(),
       ),
     );
   }
---
+++ lib/presentations/widgets/my_bottom_navbar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyButtomNavbar extends StatelessWidget {
+ // void Function(int)? onTabChange;
+
+ const MyButtomNavbar({
+    super.key,
+    //required this.onTabChange,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Para situar el navbar por debajo
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+
+        // Libreria de google_nav_bar
+        child: GNav(
+          color: Colors.grey[400],
+          activeColor: Colors.grey.shade700,
+          tabActiveBorder: Border.all(color: Colors.white),
+          tabBackgroundColor: Colors.grey.shade100,
+          mainAxisAlignment: MainAxisAlignment.center,
+          tabBorderRadius: 16,
+          gap: 8,
+          //onTabChange: (value) => onTabChange!(value),
+          
+          
+          tabs: const [
+            // Boton de icono + 'Reservar'
+
+            GButton(
+              icon: Icons.garage_outlined,
+              text: 'Reservar',
+              ),
+
+            // Boton de icono + 'Mis Reservas'  
+            GButton(
+              icon: Icons.list,
+              text: 'Mis Reservas',
+              
+              ),
+
+            // Boton de icono + 'Buscar Auto'
+            GButton(
+              icon: Icons.search,
+              text: 'Buscar Auto',
+              
+              ),
+
+
+          ],
+      
+        )
+      ),
+    );
+  }
+}
