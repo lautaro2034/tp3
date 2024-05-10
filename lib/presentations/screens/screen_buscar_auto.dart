e7176a1defa64856dd716f899c1d6e80088336c1
Alejandro Agustin Collado <alejandro.agustincollado@gmail.com>
2024-05-09 22:06:55 -0300

    Version_router_v1

---
+++ lib/presentations/screens/screen_buscar_auto.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BuscadorAuto extends StatelessWidget {
+  static const String name = 'BuscadorAuto';
+  const BuscadorAuto({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
