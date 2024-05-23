29652fe4d78783be3c50273f1a5fa7b413fa2e30
lautaro <116090828+lautaro2034@users.noreply.github.com>
2024-05-23 20:38:11 -0300

    arreglo boton

--- lib/presentations/screens/form_reserva.dart
+++ lib/presentations/screens/form_reserva.dart
@@ -1,11 +1,8 @@
-// confirm_reservation_page.dart
 import 'package:app_de_estacionamiento/presentations/screens/booking_calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class ConfirmReservationPage extends StatefulWidget {
-  //int index;
-
   const ConfirmReservationPage({super.key});
 
   @override
@@ -18,45 +15,61 @@ class _ConfirmReservationPageState extends State<ConfirmReservationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Confirmar Reserva'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Modelo',
-                border: OutlineInputBorder(),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Confirmar Reserva'),
+        ),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(30.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: IntrinsicHeight(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Modelo',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 55),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Patente',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Marca',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        ElevatedButton(
+                          onPressed: () {
+                            context.goNamed(BookingCalendarDemoApp.name);
+                          },
+                          child: const Center(child: Text('confirmar')),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 55),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Patente',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 50),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Marca',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 80),
-            ElevatedButton(
-              onPressed: () {
-                context.goNamed(BookingCalendarDemoApp.name);
-              },
-              child: const Center(child: Text('confirmar')),
-            ),
-          ]),
+            );
+          },
         ),
       ),
     );
